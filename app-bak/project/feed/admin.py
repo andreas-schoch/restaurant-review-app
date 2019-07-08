eeaf6d033b4d1812693cc5276953c2654afd3334
oliver <x@pri.la>
2019-07-08 11:58:27 +0200

    deleted app-bak

--- app-bak/project/feed/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from project.feed.models import Post, Like, FriendRequest, UserProfile
-
-admin.site.register(Post)
-admin.site.register(Like)
-admin.site.register(FriendRequest)
-admin.site.register(UserProfile)
--- app-bak/project/feed/conf.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class FeedApp(AppConfig):
-    name = 'project.feed'
-    verbose_name = "Social feed app"
-
-    def ready(self):
-        from . import signals  # noqa
