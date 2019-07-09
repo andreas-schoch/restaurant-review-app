ea8d4e95e01367a86df2bf31746778590228eaa6
x <x@pri.la>
2019-07-09 16:41:58 +0200

    edit script

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+rm -rf /frontend-build/*
+cp -r /frontend/build/* /frontend-build
 python manage.py migrate
 python manage.py collectstatic --noinput
 /opt/conda/envs/app/bin/gunicorn --bind 0.0.0.0:8000 app.wsgi
