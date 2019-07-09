91aebc3eb931791d5fea99f77414070addf13a29
x <x@pri.la>
2019-07-09 13:10:44 +0200

    docker compose deploy

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 python manage.py migrate
 python manage.py collectstatic --noinput
-/opt/conda/envs/app/bin/gunicorn --bind 0.0.0.0:8000 project.wsgi
+/opt/conda/envs/app/bin/gunicorn --bind 0.0.0.0:8000 app.wsgi
