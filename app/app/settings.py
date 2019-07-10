44881c1ee89b3da5129c3f302b556016c0891dbf
oliver hofmann <230-x@users.noreply.gitlab.propulsion-home.ch>
2019-07-10 08:19:24 +0000

    Update app/app/settings.py

--- app/app/settings.py
+++ app/app/settings.py
@@ -138,9 +138,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static-files/'
 
-MEDIA_URL = '/media/'
+MEDIA_URL = '/media-files/'
 MEDIA_ROOT = 'uploads'
 
 
