fb9174c1bca6d97f576ea203d1672dc4f6bbad04
Georgios Xonikis <giorgo.xonikis@gmail.com>
2019-07-10 10:05:38 +0200

    Endpoints: Tokens

--- app/app/settings.py
+++ app/app/settings.py
@@ -91,6 +91,16 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql_psycopg2",
+#         "NAME": os.environ.get("POSTGRES_DB"),
+#         "USER": os.environ.get("POSTGRES_USER"),
+#         "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+#         "HOST": os.environ.get("POSTGRES_HOST"),
+#         "PORT": os.environ.get("POSTGRES_PORT"),
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
